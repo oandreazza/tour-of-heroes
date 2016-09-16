d5fcaf70f2f88c6aa60bb882fbd800e111d75160
O Andreazza <mauricio.webdev@gmail.com>
2016-09-16 15:25:09 -0300

    working with Promise

--- app/app.component.js
+++ app/app.component.js
@@ -22,7 +22,8 @@ var AppComponent = (function () {
         this.selectedHero = hero;
     };
     AppComponent.prototype.getHeroes = function () {
-        this.heroes = this.heroService.getHeroes();
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
     };
     AppComponent = __decorate([
         core_1.Component({
--- app/app.component.ts
+++ app/app.component.ts
@@ -89,7 +89,7 @@ export class AppComponent implements OnInit {
 		this.selectedHero = hero;
 	}
 	getHeroes(): void{
-		this.heroes = this.heroService.getHeroes();
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
 
 
