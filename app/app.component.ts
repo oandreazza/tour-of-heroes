bcb4d044faca86bc8168ab0dc65bf14b0f0866c0
O Andreazza <mauricio.webdev@gmail.com>
2016-09-16 15:16:00 -0300

    working with Injectable Service to gathering data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 
 
@@ -83,18 +65,33 @@ styles: [`
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+ 
+ providers: [HeroService]
 
 
 
 })
 
 
-export class AppComponent { 
+export class AppComponent implements OnInit { 
 	title = 'Tour of heroes';
-	heroes = HEROES;
+	heroes: Hero[];
 	selectedHero :  Hero;
+	
+	constructor(private heroService: HeroService) { }
+
+
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
+	getHeroes(): void{
+		this.heroes = this.heroService.getHeroes();
+	}
+
+
+
 }
