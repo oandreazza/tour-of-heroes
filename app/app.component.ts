b61aa89e0fac6103f15521648cc0748ce62b4856
O Andreazza <mauricio.webdev@gmail.com>
2016-09-19 11:50:13 -0300

    adding some refactoring with css

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
 	template: `
 		 <h1>{{title}}</h1>
 		 <nav>
-		 	<a routerLink="/dashboard">Dashboard</a>
-		 	<a routerLink="/heroes">Heroes</a>
+		 	<a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+		 	<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
    		 </nav>
    		 <router-outlet></router-outlet>
 	`,
+
+	styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent{
--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -35,7 +35,8 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent = __decorate([
         core_1.Component({
             selector: 'my-hero-detail',
-            templateUrl: 'app/hero-detail.component.html'
+            templateUrl: 'app/hero-detail.component.html',
+            styleUrls: ['app/hero-detail.component.css']
         }), 
         __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.ActivatedRoute])
     ], HeroDetailComponent);
